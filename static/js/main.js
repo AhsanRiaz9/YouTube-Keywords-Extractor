059d92bfe67e47becbdb30d3e165cf785b6ac4ad
AhsanRiaz9 <bcsf18m009@pucit.edu.pk>
2021-08-04 18:41:09 +0500

    Final Commit

---
+++ static/js/main.js
@@ -0,0 +1,71 @@
+function isValidYoutubeURL(url) {
+  let patterns = [
+    "https://youtu.be/",
+    "youtu.be/",
+    "https://www.youtube.com/watch?v=",
+    "youtube.com/watch?v=",
+  ];
+  for (let i = 0; i < patterns.length; i++) {
+    if (url.indexOf(patterns[i]) == 0) {
+      // alert("Correct URL");
+      return true;
+    }
+  }
+  // alert("Invalid URL");
+  return false;
+}
+$(document).ready(function () {
+  $("#copy-tags").click(function () {
+    const area = $("#video-keywords");
+    area.select();
+    document.execCommand("copy");
+    $(".copy-status").css("display", "block");
+  });
+  $("#searchTags").click(function () {
+    let url = $("#video-url").val();
+    let flag = isValidYoutubeURL(url);
+    if (flag == false) {
+      alert("Invalid YouTube URL");
+      return;
+    }
+    // Using AJAX to get tags
+    $.ajax({
+      type: "POST",
+      url: "/getYoutubeVideoTags",
+      data: JSON.stringify({ url: url }),
+      dataType: "json",
+      async: true,
+      beforeSend: function (xhr) {
+        if (xhr && xhr.overrideMimeType) {
+          xhr.overrideMimeType("application/json;charset=utf-8");
+        }
+      },
+      dataType: "json",
+      success: function (data) {
+        //Do stuff with the JSON data
+        if (data["tagList"].length == 0) {
+          alert("No Result Found");
+          return;
+        }
+        // {#console.log(data['tagList']);#}
+        keyWords = "";
+        tagList = data["tagList"];
+        tags = "";
+        for (let i = 0; i < tagList.length; i++) {
+          keyWords =
+            keyWords +
+            ` <button class="btn btn-danger mx-2 mb-2">${tagList[i]}</button> `;
+          tags = tags + tagList[i];
+          if (i != tagList.length - 1) {
+            tags = tags + ",";
+          }
+        }
+        // {#console.log(keyWords)#}
+        document.getElementById("keywordsBtn").innerHTML = keyWords;
+        // {#alert(tags);#}
+        document.getElementById("keywordsBtn").innerHTML;
+        $("#video-keywords").val(tags);
+      },
+    });
+  });
+});
